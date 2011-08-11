f7e99d2f6c8537ab955e1f6a39a8640635e13650
Guilherme Kamizake de Freitas <guilhermekfreitas@gmail.com>
2011-08-11 17:33:39 -0300

    Removendo comentários da classe AgendamentoService

--- Doctors/src/br/com/doctors/services/AgendamentoService.java
+++ Doctors/src/br/com/doctors/services/AgendamentoService.java
@@ -24,6 +24,7 @@ public class AgendamentoService {
 	private final Minutes minutosPorConsulta;
 
 	public AgendamentoService() {
+		// pode vir parametrizado
 		inicioAtendimento = new LocalTime(8, 0);
 		fimAtendimento = new LocalTime(17, 30);
 		dataInicial = new LocalDate().plusDays(1);
@@ -33,23 +34,14 @@ public class AgendamentoService {
 
 	public List<DataInner> getHorariosDisponiveis(List<Agendamento> horariosJaPreenchidos){
 
-//		Integer duracaoConsulta = 30;
-//		String horaInicioAtendimento = "08:00";
-//		String horaFimAtendimento = "18:00";
-//		System.out.println(fmtData);
-//		System.out.println(fmtHora);
-
-		System.out.println(horariosJaPreenchidos.size());
 		Map<LocalDate, DataInner> horariosOcupados = convertToDataInner(horariosJaPreenchidos);
-		
-		
 		
 		List<DataInner> todosHorarios = new ArrayList<DataInner>();
 		
 		LocalDate dataAtual = new LocalDate(dataInicial);
 		while (!dataAtual.isAfter(dataFinal)){
 			
-			// preenche os horários para esta data.             (deixar como variaveis de instancia)
+			// preenche os horários para esta data.     
 			DataInner horariosDoDia = getHorariosDoDia(dataAtual,minutosPorConsulta);
 			
 			// se tiver esta data no horariosOcupados, elimina os horarios em comum
@@ -60,80 +52,45 @@ public class AgendamentoService {
 			
 			// adiciona novos horários
 			todosHorarios.add(horariosDoDia);
-			dataAtual = new LocalDate(dataAtual).plusDays(1);
+			dataAtual = dataAtual.plusDays(1); // vai p/ próximo dia
+//			dataAtual = new LocalDate(dataAtual).plusDays(1); // vai p/ próximo dia
 		}
 		
 		// debug
-		for (DataInner horario : todosHorarios ){
-			System.out.println(horario);
-		}
-		
+//		for (DataInner horario : todosHorarios ){
+//			System.out.println(horario);
+//		}
+//		
 		return todosHorarios;
-		
-//		for(Agendamento horario : horariosJaPreenchidos ){
-//			LocalDate dataAgendamento = horario.getDataAgendamento();
-//		}
-//		
-//		
-//		
-//		for( LocalTime horario : horariosDiarios ){
-//			System.out.println(">>" + horario);
-//		}
-//			
-//		
-//		System.out.println("Outra lista de horários: ");
-//		List<LocalTime> outraLista = new ArrayList<LocalTime>();
-//		outraLista.add(new LocalTime(10,0));
-//		outraLista.add(new LocalTime(15,0));
-//		outraLista.add(new LocalTime(16,30));
-//		
-//		System.out.println("Removendo horários não-disponíveis. Resultado:");
-//		horariosDiarios.removeAll(outraLista);
-//		for( LocalTime horario : horariosDiarios ){
-//			System.out.println(">>" + horario);
-//		}
-//		
-//		
-//		Collection<DataInner> lista = new ArrayList<DataInner>();
-		
-		// algo assim
-//		lista.removeAll(horariosJaPreenchidos);
-		
-		// pegar horários preenchidos
-		
-		// pegar lista com todos os horários
-		// tirar os horários já preenchidos.
-		
-		// retornar lista de horários p/ jsp
-		
-		
 	}
 
+	/***
+	 * 
+	 * Converte uma lista de Agendamento para um mapa de DataInner
+	 * @param horariosJaPreenchidos
+	 * @return
+	 */
 	private Map<LocalDate, DataInner> convertToDataInner(List<Agendamento> horariosJaPreenchidos) {
 
-		// pegar cada horário preenchido, e adicionar no HashMap
+		Map<LocalDate,DataInner> horarios = new HashMap<LocalDate,DataInner>();
 		
-		Map<LocalDate,DataInner> horariosOcupados = new HashMap<LocalDate,DataInner>();
-		
-//		List<DataInner> horariosOcupados = new ArrayList<DataInner>();
 		for (Agendamento agend : horariosJaPreenchidos){
 			
 			LocalDate dataAgendamento = agend.getDataAgendamento();
 			LocalTime horaAgendamento = agend.getHoraAgendamento();
 			
-			if (horariosOcupados.containsKey(dataAgendamento)){
-				// apenas adiciona mais um horário
-				DataInner data = horariosOcupados.get(dataAgendamento);
+			if (horarios.containsKey(dataAgendamento)){
+				// adiciona mais um horário
+				DataInner data = horarios.get(dataAgendamento);
 				data.addHorario(horaAgendamento.toString(fmtHora));
 			} else {
 				// deve adicionar mais uma data
 				DataInner newData = new DataInner(dataAgendamento.toString(fmtData));
 				newData.addHorario(horaAgendamento.toString(fmtHora));
-				horariosOcupados.put(dataAgendamento, newData);
+				horarios.put(dataAgendamento, newData);
 			}
 		}
-		
-		return horariosOcupados;
+		return horarios;
 	}
 
 	private DataInner getHorariosDoDia(LocalDate dataAtual, Minutes minutosPorConsulta) {
@@ -142,7 +99,6 @@ public class AgendamentoService {
 
 		DataInner diaAtual = new DataInner(dataAtual.toString(fmtData));
 		while( !horarioAtual.isAfter(fimAtendimento)){
-			
 			diaAtual.addHorario(horarioAtual.toString(fmtHora));
 			horarioAtual = new LocalTime(horarioAtual).plus(minutosPorConsulta);
 		}
